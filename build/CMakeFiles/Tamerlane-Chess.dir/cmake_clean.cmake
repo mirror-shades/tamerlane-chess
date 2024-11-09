9f915cc2d6b06f7ee5c68ac507a7b2c186d9741e
mirror-shades <logan.dembicki@gmail.com>
2024-11-09 13:39:57 -0800

    slight reorg

---
+++ build/CMakeFiles/Tamerlane-Chess.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Tamerlane-Chess.dir/link.d"
+  "CMakeFiles/Tamerlane-Chess.dir/src/main.cpp.o"
+  "CMakeFiles/Tamerlane-Chess.dir/src/main.cpp.o.d"
+  "Tamerlane-Chess"
+  "Tamerlane-Chess.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Tamerlane-Chess.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
