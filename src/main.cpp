6e21538db08fb67a3d2e65d23f3627e1c0862b09
mirror-shades <44949816+mirror-shades@users.noreply.github.com>
2024-11-11 23:21:01 -0800

    remove debug from main

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,8 @@
 #include "game.h"
-#include <iostream>
 
 int main()
 {
-    try
-    {
-        Game game;
-        game.run();
-        return 0;
-    }
-    catch (const std::exception &e)
-    {
-        std::cerr << "Error: " << e.what() << std::endl;
-        return 1;
-    }
+    Game game;
+    game.run();
+    return 0;
 }
